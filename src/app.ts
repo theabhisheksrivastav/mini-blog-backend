a873aa4f3ad74e5ccdefae132bdc0b5ce35dc58f
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2025-06-27 13:48:59 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.routes';
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());      
 
